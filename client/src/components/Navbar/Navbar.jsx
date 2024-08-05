239879d81c662d0f01106ecb7d34a33ff849d0db
Shikhar_Burman <134587780+StephenStolk@users.noreply.github.com>
2024-08-05 10:21:18 +0530

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,100 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { FaHome, FaBook, FaMoon, FaProjectDiagram, FaCommentAlt } from "react-icons/fa";
-// import { motion, AnimatePresence } from "framer-motion";
-// import { IoClose } from "react-icons/io5";
-// import { GiHamburgerMenu } from "react-icons/gi";
 
-// const Navbar = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <nav className="bg-gray-800 shadow-md fixed top-0 left-0 w-full z-10">
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//         <div className="flex items-center justify-between h-16">
-//           <div className="flex items-center">
-//             <div className="flex-shrink-0">
-//               <Link to="/" className="text-white text-lg font-semibold font-[verdana]">
-//                 DARKENSION
-//               </Link>
-//             </div>
-//             <div className="hidden md:block">
-//               <div className="ml-10 flex items-baseline space-x-4">
-//                 <NavItem to="/">Home</NavItem>
-//                 <NavItem to="/#contact">Contact</NavItem>
-//                 <NavItem to="/#services">Services</NavItem>
-//                 <NavItem to="/#appstore">Store</NavItem>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="-mr-2 flex md:hidden">
-//             <button
-//               onClick={toggleMenu}
-//               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
-//               aria-expanded="false"
-//             >
-//               {isOpen ? (
-//                 <IoClose className="text-white" />
-//               ) : (
-//                 <GiHamburgerMenu className="text-white" />
-//               )}
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//       <AnimatePresence>
-//         {isOpen && (
-//           <motion.div
-//             initial={{ opacity: 0, y: -50 }}
-//             animate={{ opacity: 1, y: 0 }}
-//             exit={{ opacity: 0, y: -50 }}
-//             className="md:hidden flex flex-col items-center justify-center w-full"
-//           >
-//             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-//               <MobileNavItem to="/">Home</MobileNavItem>
-//               <MobileNavItem to="/#contact">Contact</MobileNavItem>
-//               <MobileNavItem to="/#services">Services</MobileNavItem>
-//               <MobileNavItem to="/#appstore">Store</MobileNavItem>
-//             </div>
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
-//     </nav>
-//   );
-// };
-
-// const NavItem = ({ icon, to, children }) => {
-//   return (
-//     <Link
-//       to={to}
-//       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-//     >
-//       {icon}
-//       <span className="ml-2">{children}</span>
-//     </Link>
-//   );
-// };
-
-// const MobileNavItem = ({ to, children }) => {
-//   return (
-//     <Link
-//       to={to}
-//       className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-//     >
-//       {children}
-//     </Link>
-//   );
-// };
-
-// export default Navbar;
-
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 // import {FiMenu} from "react-icons/fi"
 import { Link } from "react-router-dom";
@@ -115,7 +19,7 @@ const Navbar = () => {
       <section className="w-full bg-brandDark md:flex items-center z-[20] justify-between fixed top-0 left-0">
         <div className="flex items-center px-2 md:px-12 py-4 justify-between">
           <span className="text-3xl font-semibold font-[Poppins] text-white/70 ml-[2rem]">
-            EcoGrow
+            ByteUnity
           </span>
 
           {open ? (
